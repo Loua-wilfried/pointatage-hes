ff286c66a81d2eba422c206914f55f73a7fdc567
Arafat koné <konedatoloyahayaarafat@gmail.com>
2025-07-21 17:17:37 +0000

    mon front

--- mobilepointage/app/index.tsx
+++ mobilepointage/app/index.tsx
@@ -163,6 +163,7 @@ const styles = StyleSheet.create({
   scrollContent: {
     flexGrow: 1,
     justifyContent: 'flex-end',
+    //position: 'relative'
   },
   container2: {
     backgroundColor: '#fff',
