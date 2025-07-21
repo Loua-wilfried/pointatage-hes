fb1efcb18666092cddc305be01d65d8db017fe15
Arafat koné <konedatoloyahayaarafat@gmail.com>
2025-07-21 16:02:28 +0000

    probleme de sur page résolue il reste 3 problème mais en rapport avec le typage

--- mobilepointage/app.config.js
+++ mobilepointage/app.config.js
@@ -1,6 +1,10 @@
-// Configuration Expo pour injecter la variable d'environnement API_BASE_URL
+import 'dotenv/config';
+
 export default ({ config }) => ({
   ...config,
+  name: "mobilepointage",
+  slug: "mobilepointage",
+  version: "1.0.0",
   extra: {
     API_BASE_URL: process.env.API_BASE_URL || 'http://172.24.1.195:8000',
   },
